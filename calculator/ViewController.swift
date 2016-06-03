3c218e8190c013e1b3bf4dfa9e8a1b762405632a
Nima M.Farahani <nima_f101@yahoo.com>
2016-06-02 21:33:56 -0400

    Initial Commit

---
+++ calculator/ViewController.swift
@@ -0,0 +1,168 @@
+//  calculator
+//  ViewController.swift
+// Nima M.Farahani
+
+
+// **app is designed for iphone 6 and 6s**
+
+
+
+import UIKit
+
+class ViewController: UIViewController, UITextFieldDelegate {
+    
+    override func shouldAutorotate() -> Bool {
+        return false
+    }
+ 
+    @IBOutlet var myImage: UIImageView!
+    @IBOutlet var showDigits: UILabel!
+    @IBOutlet var showCalculation: UILabel!
+    
+    var number1 = 0.0, number2 = 0.0, result = 0.0
+    var istyping = false, decimalPushed = false
+    var opr = ""
+    var tempRes: Double = 0.0
+    var decimalCount = 0
+    var name = "9.jpg"
+    
+    //for numbers
+    @IBAction func numbers(sender: AnyObject) {
+        
+        var number = sender.currentTitle!
+        if istyping {
+            showDigits.text = showDigits.text! + number!
+            showCalculation.text = showCalculation.text! + number!
+           
+        } else {
+            showDigits.text = number;
+            showCalculation.text = showCalculation.text! + number!
+            istyping = true;
+        }
+    }
+    
+    //decimal period
+    @IBAction func decimal(sender: AnyObject) {
+        
+        let decimal = sender.currentTitle!
+        
+        if decimalPushed{} else{
+            showDigits.text = showDigits.text! + decimal!
+            if istyping{
+                showCalculation.text = showCalculation.text! + decimal!
+            }
+            decimalPushed = true
+        }
+        decimalCount++
+    }
+    
+    //for equal sign
+    @IBAction func answer(sender: AnyObject) {
+        
+        number2 = Double(showDigits.text!)!;
+        if opr == "+"{
+            result = number1 + number2
+        }else if opr=="-"{
+            result = number1 - number2
+        }else if opr == "x"{
+            result = number1 * number2
+        }else if opr == "/"{
+            
+            tempRes = round((Double( number1) / Double(number2))*1000)/1000
+            if number1 % number2 == 0{
+                showDigits.text = "\(Int(tempRes))"
+                showCalculation.text = showCalculation.text! + " = " + "\(Int(tempRes))"
+            }else{
+                showDigits.text = "\(tempRes)"
+                showCalculation.text = showCalculation.text! + " = " + "\(tempRes)"
+            }
+        }
+        
+        if opr == "+" || opr == "-" || opr == "x"{
+            if istyping{
+                if decimalCount == 0{
+                    showDigits.text = "\(Int(result))"
+                    showCalculation.text = showCalculation.text! + " =" + " \(Int(result))"
+                
+                }else{
+                    showDigits.text = "\(result)"
+                    showCalculation.text = showCalculation.text! + " =" + " \(result)"
+                    
+                }
+            }
+        }
+    }
+    @IBAction func negativeMaker(sender: AnyObject) {
+        
+        number1 = Double(showDigits.text!)!
+        result = number1 - (number1*2)
+        if istyping{
+            if decimalCount == 0{
+                showDigits.text = "\(Int(result))"
+                showCalculation.text = "\(Int(result))"
+            }else{
+                showDigits.text = "\(result)"
+                showCalculation.text = "\(result)"
+            }
+        }
+        
+        number1 = result
+        decimalPushed = false
+    }
+    
+    @IBAction func percentage(sender: AnyObject) {
+        
+        
+        number2 = Double(showDigits.text!)!
+        
+        if opr == "+"{
+            tempRes = round(Double(number1) + ((Double(number1) * Double(number2))/100))
+            
+        }else if opr == "-"{
+            tempRes = round(Double(number1) - ((Double(number1) * Double(number2))/100))
+            
+        }else if opr == "*"{
+            tempRes = round(((Double(number1) * Double(number2))/100))
+            
+        }
+        if istyping{
+            showDigits.text = "\(tempRes)"
+            showCalculation.text = showCalculation.text! + "% = \(tempRes)"
+        }
+        istyping = false;
+        decimalPushed = false
+    }
+    
+    //to reset calc
+    @IBAction func reset(sender: AnyObject) {
+        showDigits.text = "0"
+        number1 = 0
+        number2 = 0
+        result = 0
+        opr = ""
+        decimalPushed =  false
+        showCalculation.text = ""
+        istyping = false
+    }
+    
+    @IBAction func operators(sender: AnyObject) {
+        
+        number1 = Double(showDigits.text!)!
+        opr = sender.currentTitle!!;
+        if istyping{
+            showCalculation.text = showCalculation.text! + " \(opr) "
+        }
+        istyping = false;
+        decimalPushed = false;
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.myImage.image = UIImage(named: name)
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
---
+++ calculator/wallpaperView.swift
@@ -0,0 +1,41 @@
+import Foundation
+import UIKit
+
+class wallpaperView: UIViewController{
+    
+    var name = "8.jpg"
+    
+    @IBAction func wallpaperSelect(sender: AnyObject) {
+        
+        if sender.restorationIdentifier == "1"{
+            name = "1.jpg"
+        }else if sender.restorationIdentifier == "2"{
+            name = "2.jpg"
+        }else if sender.restorationIdentifier == "3"{
+            name = "3.jpg"
+        }else if sender.restorationIdentifier == "4"{
+            name = "4.jpg"
+        }else if sender.restorationIdentifier == "5"{
+            name = "5.jpg"
+        }else if sender.restorationIdentifier == "6"{
+            name = "6.jpg"
+        }else if sender.restorationIdentifier == "7"{
+            name = "7.jpg"
+        }else if sender.restorationIdentifier == "8"{
+            name = "8.jpg"
+        }else {
+            name = "9.jpg"
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        self.view.backgroundColor = UIColor(patternImage: UIImage(named: "8.jpg")!)
+        }
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        let firstViewController = segue.destinationViewController as! ViewController
+            firstViewController.name = name
+    }
+}
